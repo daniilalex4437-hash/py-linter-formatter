6a71ade83105dbe2fa3b07d278f3173e3962231c
daniilalex4437-hash <daniilalex4437@gmail.com>
2026-05-05 18:05:57 +0200

    soluTion

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def format_linter_error(error):
+def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -8,7 +8,7 @@ def format_linter_error(error):
     }
 
 
-def format_single_linter_file(file_path, errors):
+def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
@@ -16,9 +16,8 @@ def format_single_linter_file(file_path, errors):
     }
 
 
-def format_linter_report(linter_report):
+def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
-
